2f5fe89bc258d3e0dd5ea9840f9448bcda22fb50
marciovz <marciovz@yahoo.com.br>
2023-02-11 17:09:08 -0300

    Implementado funcionalidade de inserir e listar tarefas no banco de dados

--- src/server.js
+++ src/server.js
@@ -1,10 +1,31 @@
 import http from 'node:http';
+import { randomUUID } from 'node:crypto'
+
+import { Database } from './database/index.js';
+
+const database = new Database();
 
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/tasks') {
-    return res.end('Listagem de usuários');
+    const tasks = database.select('tasks');
+    return res.end(JSON.stringify(tasks));
+  }  
+
+
+  if (method === 'POST' && url === '/tasks') {
+
+    const task = database.insert('tasks', {
+      id: randomUUID(),
+      title: 'módulo 1 de nodejs',
+      descrition: 'Terminar o módulo 1 de nodejs',
+      completed_at: null,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    return res.end(JSON.stringify(task));
   }  
 
   res.writeHead(404).end('Route not found!');
