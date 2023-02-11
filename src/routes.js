e2fa4e9ebcfae4f24ae28039f2518df3f6cc533c
marciovz <marciovz@yahoo.com.br>
2023-02-11 18:41:33 -0300

    Implementado funcionaldade de remoção de uma tarefa

--- src/routes.js
+++ src/routes.js
@@ -37,5 +37,14 @@ export const routes = [
   
       return res.end(JSON.stringify(task));
     }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params;
+      database.delete('tasks', id);
+      return res.writeHead(204).end();
+    }
   }
 ]
