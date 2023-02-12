ae098d361c4820936c263f45a13fec9d14d452e8
marciovz <marciovz@yahoo.com.br>
2023-02-12 17:12:41 -0300

    implementado a funcionalidade de importação de arquivos csv

---
+++ import-csv/index.js
@@ -0,0 +1,34 @@
+import fs from 'node:fs';
+import { parse } from 'csv-parse';
+
+const csvPath = new URL('./tasks.csv', import.meta.url);
+
+async function processFile() {
+  const parseChunk = fs
+    .createReadStream(csvPath)
+    .pipe(
+      parse({
+        delimiter: ',',
+        fromLine: 2,
+        skipEmptyLines: true,
+      })
+    );
+  
+  for await (const chunk of parseChunk){
+    const [title, description] = chunk;
+
+    await fetch('http://localhost:3333/tasks', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title, description })
+    });
+
+    await wait(1000)
+  }
+}
+
+processFile();
+
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
